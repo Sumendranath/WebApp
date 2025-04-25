2a08b215b07d6b8219a4e99db95caadaa2a0713c
Sumen <sumendran@gmail.com>
2025-04-25 17:43:37 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,18 @@
 
+// This is a simple Service Worker script
+
+// Install event
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installed.');
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated.');
+  // You can add cache management code here.
+});
+
+// Fetch event - it allows caching and fetching data from the network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
